e6a5d1f61b6178bbe842d3b2a31cdd49a9ff7606
pi22by7 <piyushairani123@outlook.com>
2023-02-03 20:34:34 +0530

    dummy navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import { NavBar } from './navbar/NavBar';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
