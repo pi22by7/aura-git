06c2f25838aae3c01e6fdc84a362fd3885d49068
Kishor Balgi <kishorbalgi@gmail.com>
2023-02-06 23:36:02 +0530

    Added Tailwind to project, updated navbar, added login and signup page

---
+++ src/Pages/EventsPage.jsx
@@ -0,0 +1,8 @@
+const EventsPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center">EventsPage</h1>
+    </div>
+  );
+};
+export default EventsPage;
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,8 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center">Home</h1>
+    </div>
+  );
+};
+export default HomePage;
