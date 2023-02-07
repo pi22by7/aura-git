69520ac1ecbc1a33a716262ac4d36a9009fb2b7f
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-02-07 15:31:28 +0530

    feat(db): setup mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
 // Setup
 require("dotenv").config();
 
+// Connect to Database
+require("./src/scripts/mongodb.script");
+
 // Start express server
 const expressApp = require("./src/utils/express.util");
