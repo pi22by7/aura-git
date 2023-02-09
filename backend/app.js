eae0830ca96134d5e772f518bbfe375e69d831b1
Parishkar Singh <79470399+parishkar-9790@users.noreply.github.com>
2023-02-09 23:40:43 +0530

    cleaned

--- backend/app.js
+++
@@ -1,8 +0,0 @@
-// Setup
-require("dotenv").config();
-
-// Connect to Database
-require("./src/scripts/mongodb.script");
-
-// Start express server
-const expressApp = require("./src/utils/express.util");
