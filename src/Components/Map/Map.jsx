d77dcf2f05629f69f10651aa68a3afe79f4c37eb
pi22by7 <piyushairani123@outlook.com>
2023-02-13 17:58:56 +0530

    d3 ACTUALLY WORKED AAAAAAAAA

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,21 +1,37 @@
 import React, { useRef, useEffect } from "react";
 import * as d3 from "d3";
 
-const Map = ({ svgMap }) => {
-  const svgRef = useRef(null);
+const Map = () => {
+  const zoomRef = useRef(null);
 
   useEffect(() => {
-    const svg = d3.select(svgRef.current);
-    svg.select("circle").on("click", function () {
-      console.log("Circle clicked");
-    });
+    const svg = d3
+      .select(zoomRef.current)
+      .append("svg")
+      .attr("width", 500)
+      .attr("height", 500);
+
+    const image = svg
+      .append("image")
+      .attr("xlink:href", "https://via.placeholder.com/500x500")
+      .attr("width", 500)
+      .attr("height", 500);
+
+    const zoom = d3
+      .zoom()
+      .scaleExtent([0.5, 10])
+      .on("zoom", (e) => {
+        image.attr("transform", e.transform);
+      });
+
+    svg.call(zoom);
+
+    return () => {
+      svg.remove();
+    };
   }, []);
 
-  return (
-    <div>
-      <svg ref={svgRef} dangerouslySetInnerHTML={{ __html: svgMap }} />
-    </div>
-  );
+  return <div ref={zoomRef} />;
 };
 
 export default Map;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,10 @@
+import Map from "../Components/Map/Map";
+
 const HomePage = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold text-center">Home</h1>
+      <Map />
     </div>
   );
 };
