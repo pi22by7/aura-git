8a5122bf1294d29d65b94e411988e811c001fc3e
pi22by7 <piyushairani123@outlook.com>
2023-02-15 21:19:13 +0530

    map changed, tried zoom again

--- src/Components/Globe/Globe.jsx
+++ src/Components/Globe/Globe.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import Globe from "globe.gl";
 import data from "./geo.json";
-import art_map from "./art_map.png";
+import art_map from "./update.png";
+// import marker from "./marker.png";
 
 const GlobeComponent = () => {
   useEffect(() => {
@@ -24,7 +25,7 @@ const GlobeComponent = () => {
       world.controls().autoRotate = true;
       world.controls().update();
     });
-    const markerSvg = `<svg viewBox="-4 0 36 36">
+    const marker = `<svg viewBox="-4 0 36 36">
       <path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
       <circle fill="black" cx="14" cy="14" r="7"></circle>
     </svg>`;
@@ -45,14 +46,14 @@ const GlobeComponent = () => {
       };
     });
 
-    const world = Globe({ animateIn: true, waitForGlobeReady: true })
+    const world = Globe({ animateIn: true, waitForGlobeReady: false })
       .globeImageUrl(art_map)
       // .pointOfView(74.50342658528442, 15.869407619709492)
       .backgroundImageUrl("//unpkg.com/three-globe/example/img/night-sky.png")
       .htmlElementsData(gData)
       .htmlElement((d) => {
         const el = document.createElement("a");
-        el.innerHTML = markerSvg;
+        el.innerHTML = marker;
         el.style.color = d.color;
         el.style.width = `${d.size}px`;
 
@@ -66,11 +67,11 @@ const GlobeComponent = () => {
         return el;
       })(document.getElementById("globeViz"));
 
-    world.controls().enableZoom = false;
+    world.controls().enableZoom = true;
     world.controls().autoRotate = true;
     world.controls().autoRotateSpeed = 1.0;
-    world.controls().maxZoom = 0;
-    world.controls().minZoom = 0;
+    // world.controls().minDistance = 100;
+    // world.controls().maxDistance = 10;
   }, []);
   return (
     <div className="w-[100vw] h-[100vh] absolute top-0">
