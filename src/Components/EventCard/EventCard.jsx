c6c75af5d2fca36f801c611331fe71b5741ec06c
Kishor Balgi <kishorbalgi@gmail.com>
2023-02-22 23:25:44 +0530

    Redesigned event card

---
+++ src/Components/EventCard/EventCard.jsx
@@ -0,0 +1,24 @@
+export const EventCard = () => {
+  return (
+    <div className="event grid md:grid-cols-2 rounded-lg bg-white m-5 p-2 lg:w-3/4 w-11/12 h-fit">
+      <div className="">
+        <img
+          src="https://cdn.pastemagazine.com/www/articles/best-of-festival-posters.jpg"
+          alt="EventArt"
+          className="self-center h-full object-contain"
+        />
+      </div>
+      <div className="event-info m-5 w-full md:text-left justify-center">
+        <h1 className="text-3xl font-bold">Title</h1>
+        <h3 className="text-xl font-semibold">Tagline</h3>
+        <h3 className="text-xl mt-5 font-semibold">Description:</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
+          aperiam voluptatem nesciunt accusamus amet, veniam sunt rem soluta
+          possimus corporis tenetur temporibus velit, autem eaque architecto
+          nobis tempora officiis molestiae.
+        </p>
+      </div>
+    </div>
+  );
+};
--- src/Pages/EventsPage.jsx
+++ src/Pages/EventsPage.jsx
@@ -1,11 +1,14 @@
-import { EventPlaceholder } from "../Components/EventPlaceholder/EventPlaceholder";
+import { EventCard } from "../Components/EventCard/EventCard";
 
 const EventsPage = () => {
   return (
-    <div className="grid grid-cols-1 justify-items-center bg-events bg-contain bg-no-repeat">
+    <div className="h-[100vh] mt-16 bg-events bg-contain bg-no-repeat overflow-scroll [&::-webkit-scrollbar]:hidden">
       <h1 className="text-3xl font-bold text-center">Events</h1>
-      <br />
-      <EventPlaceholder />
+      <div className="grid grid-cols-1 justify-items-center">
+        <EventCard />
+        <EventCard />
+        <EventCard />
+      </div>
     </div>
   );
 };
