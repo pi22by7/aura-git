9071a3db0198d829801cbc6fe3027143490944aa
pi22by7 <piyushairani123@outlook.com>
2023-02-25 11:47:24 +0530

    preloader logo

--- src/Components/Globe/PreLoader/PreLoader.jsx
+++ src/Components/Globe/PreLoader/PreLoader.jsx
@@ -2,8 +2,8 @@ import logo from "../../../Assets/logo.png";
 const PreLoader = () => {
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-white absolute top-0">
-      <img src={logo} className="h-32 mr-3 sm:h-16" alt="Aura Logo" />
-      <p className="text-2xl text-black text-center">Loading...</p>
+      <img src={logo} className="h-44 mr-3 sm:h-16" alt="Aura Logo" />
+      {/* <p className="text-2xl text-black text-center">Loading...</p> */}
     </div>
   );
 };
