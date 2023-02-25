61b4f6603084289701ee5931a3fff2923132c9da
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-02-25 20:24:02 +0530

    fix: cors

--- backend/utils/express.util.js
+++ backend/utils/express.util.js
@@ -1,6 +1,7 @@
 // Imports
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 // TODO: Must use `https` later
 const http = require("http");
@@ -14,6 +15,7 @@ const { PORT } = process.env;
 // Body
 // expressApp.use(bodyParser.json())
 expressApp.use(express.json());
+expressApp.use(cors());
 expressApp.use(cookieParser());
 
 httpApp.listen(PORT, () => console.log("[express.util] Express server running on port", PORT));
