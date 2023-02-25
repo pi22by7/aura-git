1e088b98c1f1064f28f4929fef8705bfb31311bf
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-02-25 17:06:49 +0530

    refactor: include dark templates

--- backend/utils/templates.util.js
+++ backend/utils/templates.util.js
@@ -7,6 +7,9 @@ const meta = require("../configs/meta.json");
 const emailVerificationTemplate = path.join(__dirname, "..", "templates", "email.verification.rendered.template.html");
 const passwordResetTemplate = path.join(__dirname, "..", "templates", "password.reset.rendered.template.html");
 
+const emailVerificationDarkTemplate = path.join(__dirname, "..", "templates", "email.verification.dark.template.html");
+const passwordResetDarkTemplate = path.join(__dirname, "..", "templates", "password.reset.dark.template.html");
+
 // Body
 const chooseEmailVerificationTemplate = async function ({ user_name = "", redirect_uri = "#", reply_mail = meta.emails.reply_email }) {
 	try {
@@ -55,7 +58,56 @@ const choosePasswordResetTemplate = async function ({ user_name = "", dismiss_ur
 	return null;
 };
 
+const chooseEmailVerificationDarkTemplate = async function ({ user_name = "", redirect_uri = "#", reply_mail = meta.emails.reply_email }) {
+	try {
+		let html = (await new Promise((resolve, reject) => {
+			fs.readFile(emailVerificationDarkTemplate, (error, data) => {
+				if (error)
+					return reject(error);
+				
+				resolve(data);
+			});
+		})).toString();
+
+		html = html.split("{{user_name}}").join(user_name)
+			.split("{{redirect_uri}}").join(redirect_uri)
+			.split("{{reply_email}}").join(reply_mail);
+		
+		return html;
+	} catch (error) {
+		console.error("[templates.util]", error);
+	}
+
+	return null;
+};
+
+const choosePasswordResetDarkTemplate = async function ({ user_name = "", dismiss_uri = "#", redirect_uri = "#", reply_mail = meta.emails.reply_email }) {
+	try {
+		let html = (await new Promise((resolve, reject) => {
+			fs.readFile(passwordResetDarkTemplate, (error, data) => {
+				if (error)
+					return reject(error);
+				
+				resolve(data);
+			});
+		})).toString();
+
+		html = html.split("{{user_name}}").join(user_name)
+			.split("{{dismiss_uri}}").join(dismiss_uri)
+			.split("{{redirect_uri}}").join(redirect_uri)
+			.split("{{reply_email}}").join(reply_mail);
+		
+		return html;
+	} catch (error) {
+		console.error("[templates.util]", error);
+	}
+
+	return null;
+};
+
 module.exports = {
 	chooseEmailVerificationTemplate,
 	choosePasswordResetTemplate,
+	chooseEmailVerificationDarkTemplate,
+	choosePasswordResetDarkTemplate,
 };
