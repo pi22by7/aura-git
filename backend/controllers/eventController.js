1f244b84f751c83e578f7f601e05c2fc36e2df97
Kishor Balgi <kishorbalgi@gmail.com>
2023-02-25 20:19:01 +0530

    grouped events by club

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -3,29 +3,36 @@ const Event = require("../models/Event");
 const Response = require("../models/standard.response.model");
 
 module.exports.event_get = async (req, res) => {
-    try {
-        const event = await Event.findOne({ _id: req.params.id });
-        if (!event)
-            return res.status(404).send(Response(errors[404].eventNotFound));
+  try {
+    const event = await Event.findOne({ _id: req.params.id });
+    if (!event) return res.status(404).send(Response(errors[404].eventNotFound));
 
-        res.json(Response(false, event));
-    } catch (error) {
-        console.error("[eventController]", error);
+    res.json(Response(false, event));
+  } catch (error) {
+    console.error("[eventController]", error);
 
-        if ("message" in error)
-            return res.status(500).json(Response(error.message));
-        return res.status(500).send(Response(errors[500]));
-    }
+    if ("message" in error) return res.status(500).json(Response(error.message));
+    return res.status(500).send(Response(errors[500]));
+  }
 };
 module.exports.allevent_get = async (req, res) => {
-    try {
-        const events = await Event.find();
-        res.json(Response(false, events));
-    } catch (error) {
-        console.error("[eventController]", error);
+  try {
+    const events = await Event.aggregate([
+      {
+        $group: {
+          _id: "$club",
+          events: {
+            $push: "$$ROOT",
+          },
+        },
+      },
+    ]);
 
-        if ("message" in error)
-            return res.status(500).json(Response(error.message));
-        return res.status(500).send(Response(errors[500]));
-    }
+    res.json(Response(false, events));
+  } catch (error) {
+    console.error("[eventController]", error);
+
+    if ("message" in error) return res.status(500).json(Response(error.message));
+    return res.status(500).send(Response(errors[500]));
+  }
 };
