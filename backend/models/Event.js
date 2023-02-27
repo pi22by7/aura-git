3387c5ec0817335afecd66b0b2d970fc9a3289d2
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-02-27 22:50:56 +0530

    refactor: loose validation and slugs

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -1,50 +1,74 @@
 // Imports
 const mongoose = require("mongoose");
+const eventConfig = require("../configs/event.config.json");
+const { sluggify } = require("../utils/utils");
 
 // Body
 const EventSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    club: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    team_size: {
-        type: Number,
-        required: true,
-    },
-    rounds: {
-        type: Number,
-        required: true,
-    },
-    registration_limit: {
-        type: String,
-        required: true,
-    },
-    rules: {
-        type: [String],
-        required: true,
-    },
-    event_coordinators: [{
-        name: {
-            type: String,
-            required: true,
-        },
-        contact_number: {
-            type: String,
-            required: true,
-        },
-        image: {
-            type: String,
-            required: true,
-        },
-    }],
+	kind: {
+		type: String,
+		enum: Object.values(eventConfig.kinds),
+		default: eventConfig.defaultKind,
+	},
+	title: {
+		type: String,
+		trim: true,
+		unique: true,
+		required: true,
+	},
+	club: {
+		type: String,
+		trim: true,
+		required: true,
+	},
+	description: {
+		type: String,
+		trim: true,
+	},
+	team_size: {
+		type: Number,
+	},
+	rounds: {
+		type: Number,
+	},
+	registration_limit: {
+		type: String,
+		trim: true,
+	},
+	rules: {
+		type: [String],
+	},
+	event_coordinators: [{
+		name: {
+			type: String,
+			trim: true,
+		},
+		contact_number: {
+			type: String,
+			trim: true,
+		},
+		image: {
+			type: String,
+			trim: true,
+		},
+	}],
+	_slugs: {
+		title: {
+			type: String,
+			unique: true,
+			trim: true,
+			default: function () {
+				return sluggify(this.title);
+			},
+		},
+		club: {
+			type: String,
+			trim: true,
+			default: function () {
+				return sluggify(this.club);
+			}
+		},
+	},
 });
 
 const Event = mongoose.model("event", EventSchema);
