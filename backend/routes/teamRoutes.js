e90e778dc11646eca3d6b50f33c18c4785b10bc3
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-03-03 22:58:59 +0530

    refactor: disable team deletion

--- backend/routes/teamRoutes.js
+++ backend/routes/teamRoutes.js
@@ -1,6 +1,6 @@
 // Imports
 const { Router } = require("express");
-const { complete } = require("../controllers/controllers");
+const { complete, outOfOrder } = require("../controllers/controllers");
 const teamController = require("../controllers/teamController");
 const { checkUser, requireVerifiedAuth } = require("../middleware/authMiddleware");
 
@@ -15,6 +15,7 @@ router.post("/createteam", requireVerifiedAuth, teamController.createTeam, compl
 
 router.patch("/:id", requireVerifiedAuth, teamController.modifyTeam, complete);
 
-router.delete("/:id", requireVerifiedAuth, teamController.deleteTeam, complete);
+// router.delete("/:id", requireVerifiedAuth, teamController.deleteTeam, complete);
+router.delete("/:id", outOfOrder);
 
 module.exports = router;
