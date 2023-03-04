4068f3804ac777053a7efcd6bed8393f05bd5fdd
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-03-04 21:30:06 +0530

    refactor: enable ssl

--- backend/utils/express.util.js
+++ backend/utils/express.util.js
@@ -2,14 +2,22 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 const meta = require("../configs/meta.json");
 
 // TODO: Must use `https` later
 const http = require("http");
+const https = require("https");
 
 // Constants
+const options = {
+  cert: fs.readFileSync(path.join(__dirname, "..", "cert.pem")),
+  key: fs.readFileSync(path.join(__dirname, "..", "key.pem")),
+};
+
 const expressApp = express();
-const httpApp = http.createServer(expressApp);
+const httpsApp = https.createServer(options, expressApp);
 const { PORT } = process.env;
 
 // Body
@@ -23,9 +31,10 @@ expressApp.use(
 );
 expressApp.use(cookieParser());
 
-httpApp.listen(PORT, () => console.log("[express.util] Express server running on port", PORT));
+// httpApp.listen(PORT, () => console.log("[express.util] Express server running on port", PORT));
+httpsApp.listen(PORT, () => console.log("[express.util] Express server running on port", PORT));
 
 module.exports = {
   expressApp,
-  httpApp,
+  httpsApp,
 };
