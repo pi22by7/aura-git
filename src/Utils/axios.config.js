1a58d22581adaa0b8c15b7a9d9a38b0c7f7b7794
Kishor Balgi <kishorbalgi@gmail.com>
2023-03-04 22:12:16 +0530

    https backend

--- src/Utils/axios.config.js
+++ src/Utils/axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://13.200.6.251:3001",
+  baseURL: "https://13.200.6.251:3001",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
