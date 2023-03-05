83e8ffccb5b23223c75c90bd23fa551236d92c06
Kishor Balgi <kishorbalgi@gmail.com>
2023-03-05 15:29:17 +0530

    Rulebook updates

--- src/Components/Rulebook/Rulebook.jsx
+++ src/Components/Rulebook/Rulebook.jsx
@@ -3,7 +3,7 @@ const Rulebook = () => {
     <div className="grid lg:grid-cols-12 md:grid-cols-5 grid-cols-1 h-[100vh] bg-rulebookc w-screen user-none bg-rulebook md:bg-contain bg-no-repeat bg-cover md:bg-left bg-right bg-scroll overflow-scroll [&::-webkit-scrollbar]:hidden md:px-10 px-6 py-5">
       <div className="w-full h-full relative lg:col-start-6 md:col-start-2 lg:col-span-7 md:col-span-3 col-span-1  mx-auto">
         <iframe
-          src="https://drive.google.com/file/d/10hPUDlX0J1Ubncq-WPFWqLPhK-galBg8/preview"
+          src="https://drive.google.com/file/d/1rY2VcAmbpvWIKen1cwBCAMcg4rbaCuhg/preview"
           width="100%"
           height="100%"
           allow="autoplay"
