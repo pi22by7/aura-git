9bea75762e542bc50d0e635aec46891d3150c50f
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-03-06 15:58:41 +0530

    refactor: dev environment

--- backend/utils/express.util.js
+++ backend/utils/express.util.js
@@ -25,7 +25,7 @@ const { PORT } = process.env;
 expressApp.use(express.json());
 expressApp.use(
   cors({
-    origin: meta.host,
+    origin: meta.dev_host,
     credentials: true,
   })
 );
--- src/Utils/axios.config.js
+++ src/Utils/axios.config.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://localhost:3001",
-  baseURL: "http://3.110.70.229:3001",
+  baseURL: "http://localhost:3001",
+  // baseURL: "http://3.110.70.229:3001",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
