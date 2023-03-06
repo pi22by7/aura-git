9dc02dfa4dbaa7af395b51ec57e152bb99239110
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-03-06 17:48:10 +0530

    fix: new changes

--- src/Utils/axios.config.js
+++ src/Utils/axios.config.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:3001",
-  baseURL: "http://3.110.70.229:3001",
+  // baseURL: "http://3.110.70.229:3001",
+  baseURL: "https://aura.git.edu",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
