982e5af2bcfe5f2c41f02dbdb09a3fef6de36525
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-03-06 13:16:22 +0530

    refactor: disable sign up api

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,15 @@
 // Imports
 const { Router } = require("express");
 const { checkUser } = require("../middleware/authMiddleware");
-const { complete } = require("../controllers/controllers");
+const { complete, outOfOrder } = require("../controllers/controllers");
 const authController = require("../controllers/authController");
 
 // Constants
 const router = Router();
 
 // Body
-router.post("/signup", authController.signup_post, complete);
+// router.post("/signup", authController.signup_post, complete);
+router.post("/signup", outOfOrder);
 router.post("/login", authController.login_post, complete);
 router.get("/logout", authController.logout_get, complete);
 
