f74e99452a437748e9b5a092e073f4f98191fd6a
Kishor Balgi <kishorbalgi@gmail.com>
2023-03-14 23:26:42 +0530

    Update: Literary club video added

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,24 +3,22 @@ import GlobeComponent from "../Components/Globe/Globe";
 import { messageToast } from "../Utils/Toasts/Toasts";
 
 const HomePage = () => (
-  useEffect(() => {
-    messageToast(
-      "Online Registration will close on 14 March 5:00 PM",
-      "bottom-left",
-      5000
-    );
-    messageToast(
-      "No registrations will be accepted if the payment is not completed",
-      "bottom-left",
-      5000
-    );
-  }, []),
-  (
-    <div>
-      {/* <Map /> */}
-      {/* <h1 className="text-3xl font-bold text-center">Home</h1> */}
-      <GlobeComponent />
-    </div>
-  )
+  // useEffect(() => {
+  //   messageToast(
+  //     "Online Registration will close on 14 March 5:00 PM",
+  //     "bottom-left",
+  //     5000
+  //   );
+  //   messageToast(
+  //     "No registrations will be accepted if the payment is not completed",
+  //     "bottom-left",
+  //     5000
+  //   );
+  // }, []),
+  <div>
+    {/* <Map /> */}
+    {/* <h1 className="text-3xl font-bold text-center">Home</h1> */}
+    <GlobeComponent />
+  </div>
 );
 export default HomePage;
